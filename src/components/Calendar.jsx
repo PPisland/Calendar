0b42ce800c22e245ff26fcdfc7d69cc0fa71538d
ppisland <a1365gkqls@gmail.com>
2023-04-03 17:59:36 +0900

    props

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,17 @@
+import Date from "./Date";
+import calendarData from "../calendarData.json";
+
+function Calendar() {
+  return (
+    <div className="bg-white w-1/2 rounded-lg shadow-2xl p-8">
+      <div className="pb-8 text-4xl font-bold">2023, 04</div>
+      {calendarData.map((v, i) => {
+        console.log(`${i + 1} ${v}`);
+        return <Date key={i} date={v.date} day={v.day} todos={v.todos} />;
+      })}
+      {/* <Date /> */}
+    </div>
+  );
+}
+
+export default Calendar;
---
+++ src/components/Date.jsx
@@ -0,0 +1,28 @@
+function Date(props) {
+  console.log(props);
+  return (
+    <li className="inline-block bg-white rounded-lg shadow-md">
+      <div
+        className={`${
+          props.day === "SUN"
+            ? "bg-red-400"
+            : props.day === "SAT"
+            ? "bg-orange-400"
+            : "bg-blue-400"
+        } rounded-t-lg px-2 py-1 text-xl text-white flex justify-between`}
+      >
+        <div>{props.date}</div>
+        <div>{props.day}</div>
+      </div>
+      <ul className="p-2 text-lg">
+        {props.todos.map((v, i) => {
+          return <li>{v}</li>;
+        })}
+        {/* <li>🧹 청소하기</li>
+        <li>👕 빨래하기</li> */}
+      </ul>
+    </li>
+  );
+}
+
+export default Date;
